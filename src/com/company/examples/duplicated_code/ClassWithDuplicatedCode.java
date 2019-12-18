a6811ba62088881fe43bc5d7e742b57d4b74501c
stas <stas.zabramnyi@gmail.com>
2019-12-18 17:43:19 +0200

    init commit

--- src/com/company/examples/duplicated_code/ClassWithDuplicatedCode.java
+++ src/com/company/examples/duplicated_code/ClassWithDuplicatedCode.java
@@ -5,11 +5,6 @@ public class ClassWithDuplicatedCode {
     private static int[] array = {5, 6, 2, 7, 1, 64, 47};
 
     public static int getMin() {
-        bubbleSort();
-        return array[0];
-    }
-
-    private static void bubbleSort() {
         for (int i = 0; i < array.length - 1; i++)
             for (int j = 0; j < array.length - i - 1; j++)
                 if (array[j] > array[j + 1]) {
@@ -17,10 +12,17 @@ public class ClassWithDuplicatedCode {
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
+        return array[0];
     }
 
     public static int getMax() {
-        bubbleSort();
+        for (int i = 0; i < array.length - 1; i++)
+            for (int j = 0; j < array.length - i - 1; j++)
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
         return array[array.length - 1];
     }
 }
