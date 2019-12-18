f3d9e7e16dd4f7895e4941ff29f2a310cd3753a8
stas <stas.zabramnyi@gmail.com>
2019-12-18 18:24:25 +0200

    init commit

--- src/com/company/examples/SwitchExpressions.java
+++ src/com/company/examples/SwitchExpressions.java
@@ -3,27 +3,14 @@ package com.company.examples;
 public class SwitchExpressions {
 
     private void returnNumberName(int number) {
-        String numberName;
-        switch (number) {
-            case 1:
-                numberName = "one";
-                break;
-            case 2:
-                numberName = "two";
-                break;
-            case 3:
-                numberName = "three";
-                break;
-            case 4:
-                numberName = "four";
-                break;
-            case 5:
-                numberName = "five";
-                break;
-            default:
-                numberName = "I don`t know";
-                break;
-        }
+        String numberName = switch (number) {
+            case 1 -> "one";
+            case 2 -> "two";
+            case 3 -> "three";
+            case 4 -> "four";
+            case 5 -> "five";
+            default -> "I don`t know";
+        };
     }
 
 }
--- src/com/company/examples/duplicated_code/ClassWithDuplicatedCode.java
+++ src/com/company/examples/duplicated_code/ClassWithDuplicatedCode.java
@@ -5,17 +5,11 @@ public class ClassWithDuplicatedCode {
     private static int[] array = {5, 6, 2, 7, 1, 64, 47};
 
     public static int getMin() {
-        for (int i = 0; i < array.length - 1; i++)
-            for (int j = 0; j < array.length - i - 1; j++)
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
+        bubbleSort();
         return array[0];
     }
 
-    public static int getMax() {
+    private static void bubbleSort() {
         for (int i = 0; i < array.length - 1; i++)
             for (int j = 0; j < array.length - i - 1; j++)
                 if (array[j] > array[j + 1]) {
@@ -23,6 +17,10 @@ public class ClassWithDuplicatedCode {
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
+    }
+
+    public static int getMax() {
+        bubbleSort();
         return array[array.length - 1];
     }
 }
